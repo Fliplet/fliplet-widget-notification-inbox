32554e2ff00f02de83480612663e3e0e083ec0d3
Tony Wu <twu@fliplet.com>
2020-02-24 12:23:26 +0000

    Design update

--- js/build.templates.js
+++ js/build.templates.js
@@ -3,7 +3,7 @@ this["Fliplet"]["Widget"] = this["Fliplet"]["Widget"] || {};
 this["Fliplet"]["Widget"]["Templates"] = this["Fliplet"]["Widget"]["Templates"] || {};
 
 this["Fliplet"]["Widget"]["Templates"]["templates.loadMore"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"load-more\">\n  <button data-load-more class=\"btn btn-link btn-block btn-lg\">Load more</button>\n  <i class=\"fa fa-spinner fa-pulse fa-3x fa-fw\"></i>\n</div>\n";
+    return "<div class=\"load-more\">\n  <button data-load-more class=\"btn btn-link btn-block\">Load more</button>\n  <i class=\"fa fa-spinner fa-pulse fa-3x fa-fw\"></i>\n</div>\n";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.noNotifications"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
@@ -11,7 +11,7 @@ this["Fliplet"]["Widget"]["Templates"]["templates.noNotifications"] = Handlebars
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.notification"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"notification {{#if readStatus}}notification-read{{else}}notification-unread{{/if}}{{#if hasLink}} notification-linked{{/if}}\" data-notification-id=\"{{id}}\" data-notification-status=\"{{status}}\">\n  <div class=\"subtitle\">{{moment orderAt calendar=\"null\"}}</div>\n  {{#if data.title}}<h2 class=\"title\">{{data.title}}</h2>{{/if}}\n  {{#if data.message}}<div class=\"description\">\n    <p>{{data.message}}</p>\n  </div>{{/if}}\n  {{#unless readStatus}}<div class=\"notification-badge\"></div>{{/unless}}\n</div>";
+    return "<div class=\"notification {{#if readStatus}}notification-read{{else}}notification-unread{{/if}}{{#if hasLink}} has-action{{/if}}\" data-notification-id=\"{{id}}\" data-notification-status=\"{{status}}\">\n  {{#if data.title}}<h2 class=\"title\">{{#unless readStatus}}<div class=\"notification-badge\"></div>{{/unless}}{{data.title}}</h2>{{/if}}\n  {{#if data.message}}<div class=\"description\">{{data.message}}</div>{{/if}}\n  {{#if hasLink}}<a class=\"action\" href=\"#\">See more</a>{{/if}}\n  <div class=\"subtitle\" title=\"{{moment orderAt format=\"YYYY-MM-DD hh:mm:ss A\"}}\">{{moment orderAt calendar=\"null\"}}</div>\n</div>";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.notificationsError"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
@@ -19,7 +19,7 @@ this["Fliplet"]["Widget"]["Templates"]["templates.notificationsError"] = Handleb
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.toolbar"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<p><span class=\"unread-count\">{{count}}</span> unread <span class=\"toolbar-read-all\">(<a data-read-all href=\"#\">Mark all as read</a>)</span><i class=\"fa fa-lg fa-fw fa-cog pull-right\" data-settings></i><i class=\"fa fa-lg fa-fw fa-refresh pull-right\" data-refresh></i></p>\n";
+    return "<p><span class=\"toolbar-read-all\"><a data-read-all href=\"#\">Mark all as read</a></span><i class=\"fa fa-lg fa-fw fa-cog pull-right\" data-settings></i><i class=\"fa fa-lg fa-fw fa-refresh pull-right\" data-refresh></i></p>\n";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.toolbar.empty"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
