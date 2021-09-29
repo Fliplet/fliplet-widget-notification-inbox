b83587eec8d52382e7e59981a058fafdcfc12a7e
OleksandrLavrynenko <oleksandr.lav@upplabs.com>
2021-09-29 15:38:32 +0300

    [FIX] fix translations

--- js/build.templates.js
+++ js/build.templates.js
@@ -12,9 +12,9 @@ this["Fliplet"]["Widget"]["Templates"]["templates.noNotifications"] = Handlebars
     var alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3=container.escapeExpression;
 
   return "<div class=\"no-notifications\">\r\n  <h3>"
-    + alias3((helpers.T || (depth0 && depth0.T) || alias2).call(alias1,"widgets.notificationInbox.noNotification.title",{"name":"T","hash":{},"data":data}))
+    + alias3((helpers.T || (depth0 && depth0.T) || alias2).call(alias1,"widgets.notificationInbox.noNotifications.title",{"name":"T","hash":{},"data":data}))
     + "</h3>\r\n  <p>"
-    + alias3((helpers.T || (depth0 && depth0.T) || alias2).call(alias1,"widgets.notificationInbox.noNotification.instruction",{"name":"T","hash":{},"data":data}))
+    + alias3((helpers.T || (depth0 && depth0.T) || alias2).call(alias1,"widgets.notificationInbox.noNotifications.instruction",{"name":"T","hash":{},"data":data}))
     + "</p>\r\n</div>\r\n";
 },"useData":true});
 
@@ -24,7 +24,7 @@ this["Fliplet"]["Widget"]["Templates"]["templates.notification"] = Handlebars.te
 
 this["Fliplet"]["Widget"]["Templates"]["templates.notificationsError"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     return "<div class=\"notifications-error\">\r\n  <h3>"
-    + container.escapeExpression((helpers.T || (depth0 && depth0.T) || helpers.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),"widgets.notificationInbox.notificationError",{"name":"T","hash":{},"data":data}))
+    + container.escapeExpression((helpers.T || (depth0 && depth0.T) || helpers.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),"widgets.notificationInbox.errors.loadingFailed",{"name":"T","hash":{},"data":data}))
     + "</h3>\r\n</div>\r\n";
 },"useData":true});
 
